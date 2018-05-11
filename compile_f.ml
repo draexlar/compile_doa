01967312e41631661d83b068ee190259e52ff0ea
André Trindade <andreduartetrindade@gmail.com>
2018-05-11 20:05:46 +0100

    added compile implementation and tests file

---
+++ compile_f.ml
@@ -0,0 +1,190 @@
+type doa = { 
+	states: string list; 
+	choices: string list; 
+	methods: string list; 
+	labels: string list; 
+	start: string; 
+	final: string list; 
+	mTransitions: t_elem list; 
+	lTransitions: t_elem list
+}
+and t_elem = {
+	init: string;
+	trans: string;
+	fin: string;
+};;
+
+type automaton = Nil | DOA of doa;;
+
+
+type typestate = eState list
+and eState = { 
+	name: string; 
+	transitions: transition list 
+}
+and transition = { 
+	op: string; 
+	result: w 
+}
+and w = 
+	| NextState of string
+	| Option of option list
+	| InnerState of iState
+and option = {
+	label: string;
+	state: w;
+}
+and iState = transition list
+;;
+
+
+let c = ref 0;;
+let i = ref 0;;
+
+let next_val p = 
+	p := (!p) + 1;
+		!p
+;;
+
+
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "open"; result = Option [{ label = "OK"; state = NextState "Open" }; { label = "ERROR"; state = NextState "end" }] } ] 
+	};
+	{ name = "Open"; transitions = 
+		[ { op = "hasNext"; result = Option [{ label = "TRUE"; state = NextState "Read" }; { label = "FALSE"; state = NextState "Close" }] }; 
+			{ op = "close"; result = NextState "end" } ] 
+	};
+	{ name = "Read"; transitions = 
+		[ { op = "read"; result = NextState "Open" } ] 
+	};
+	{ name = "Close"; transitions = 
+		[ { op = "close"; result = NextState "end" } ] 
+	};
+];;
+
+
+let rec belongs x l =
+	match l with
+	| [] -> false
+	| hd::tl ->
+		if x = hd then true
+		else belongs x tl
+;;
+
+let rec listUnion l1 l2 =
+	match l1 with
+	| [] -> l2
+	| x::xs ->
+		if belongs x l2 then listUnion xs l2
+		else x::listUnion xs l2
+;;
+
+let rec availableStates ts =
+	match ts with
+	| [] -> ["end"]
+	| s::next -> 
+		let v = (availableStates next) in
+			if s.name = "end" then failwith "State with name 'end' not allowed."
+			else if belongs s.name v then let err = "Found duplicate state: "^s.name in failwith err
+			else s.name::v
+;;
+let avs = ref (availableStates ts);;
+
+let add_avs s =
+	avs := s::!avs
+;; 
+
+let rec duplicateTrans t =
+	match t with
+	| [] -> []
+	| x::xs ->
+		let sl = (findPairs xs) in
+			if belongs (x.init, x.trans) sl then let err = "Found duplicate method or label: "^x.trans in failwith err
+			else x::(duplicateTrans xs)
+and findPairs l =
+	match l with
+	| [] -> []
+	| y::ys -> (y.init, y.trans)::(findPairs ys)
+;;
+
+
+let union b a =
+	match b, a with
+	| Nil, Nil -> Nil
+	| Nil, DOA d
+	| DOA d, Nil -> DOA { states = listUnion d.states ["end"]; choices = d.choices; methods = d.methods; labels = d.labels; start = d.start; 
+												final = listUnion d.final ["end"]; mTransitions = d.mTransitions; lTransitions = d.lTransitions }
+	| DOA b, DOA a -> DOA { states = listUnion a.states b.states; choices = listUnion a.choices b.choices; methods = listUnion a.methods b.methods;
+											labels = listUnion a.labels b.labels; start = a.start; final = listUnion a.final b.final; 
+											mTransitions = duplicateTrans (a.mTransitions @ b.mTransitions); lTransitions = duplicateTrans (a.lTransitions @ b.lTransitions) }
+;;
+
+
+let rec compileTypestate t =
+	match t with
+	| [] -> Nil
+	| a::body -> union (compileTypestate body) (compileStateDef a)
+
+
+and compileStateDef s =
+	match s.transitions with
+	| [] -> DOA { states = [s.name]; choices = []; methods = []; labels = []; start = s.name; final = [s.name]; mTransitions = []; lTransitions = [] }
+	| x::xs -> compileState s.name x xs
+
+
+and compileState name first next =
+	match next with
+	| [] -> compileMethod name first
+	| x::xs -> union (compileState name x xs) (compileMethod name first)
+
+
+and compileMethod name m =
+	match m.result with
+	| NextState "{}"
+	| NextState "end" -> DOA { states = [name; "end"]; choices = []; methods = [m.op]; labels = []; start = name; 
+												final = ["end"]; mTransitions = [{ init = name; trans = m.op; fin = "end" }]; lTransitions = [] }
+	| NextState next -> if (belongs next !avs) then
+													DOA { states = if name = next then [name] else [name;next]; choices = []; methods = [m.op]; labels = []; start = name;
+																final = []; mTransitions = [{ init = name; trans = m.op; fin = next }]; lTransitions = [] }
+											else let err = "Undefined state: "^next in failwith err
+	| InnerState inner -> let next = "inner"^string_of_int (next_val i) in
+														(add_avs next; let trans = { op = m.op; result = NextState next } and nextState = { name = next; transitions = inner } in
+																	union (compileStateDef nextState) (compileMethod name trans) )
+	| Option opt -> compileOptions name m.op opt
+
+
+and compileOptions name met options =
+	match options with
+	| [] -> invalid_arg "There must be at least an option"
+	| o::tl -> let choice = "choice"^string_of_int (next_val c) in 
+											let a = DOA { states = [name]; choices = [choice]; methods = [met]; labels = []; start = name; 
+													final = []; mTransitions = [{ init = name; trans = met; fin = choice }]; lTransitions = [] } in
+															union (compileLabelOptions choice o tl) a
+
+
+and compileLabelOptions name first next =
+	match next with
+	| [] -> compileLabel name first
+	| nxt::tl -> union (compileLabelOptions name nxt tl) (compileLabel name first)
+
+
+and compileLabel name opt =
+	let l = opt.label and s = opt.state in
+	match s with
+	| NextState "{}"
+	| NextState "end" -> DOA { states = ["end"]; choices = [name]; methods = []; labels = [l]; start = ""; 
+														final = ["end"]; mTransitions = []; lTransitions = [{ init = name; trans = l; fin = "end" }] }
+	| NextState next -> if (belongs next !avs) then
+												DOA { states = [next]; choices = [name]; methods = []; labels = [l]; start = ""; 
+															final = []; mTransitions = []; lTransitions = [{ init = name; trans = l; fin = next }] }
+											else let err = "Undefined state: "^next in failwith err	
+	| InnerState inner -> let next = "inner"^string_of_int (next_val i) in
+														(add_avs next; let nextState = { name = next; transitions = inner } and
+																option = { label = l; state = NextState next } in
+																		union (compileStateDef nextState) (compileLabel name option) )
+	| Option _ -> failwith "Internal choice states must always transition to external choice states."
+;;
+
+availableStates ts;;
+compileTypestate ts;;
---
+++ tests.ml
@@ -0,0 +1,254 @@
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "open"; result = Option [{ label = "OK"; state = NextState "Open" }; { label = "ERROR"; state = NextState "end" }] } ] 
+	};
+	{ name = "Open"; transitions = 
+		[ { op = "hasNext"; result = Option [{ label = "TRUE"; state = NextState "Read" }; { label = "FALSE"; state = NextState "Close" }] }; 
+			{ op = "close"; result = NextState "end" } ] 
+	};
+	{ name = "Read"; transitions = 
+		[ { op = "read"; result = NextState "Open" } ] 
+	};
+	{ name = "Close"; transitions = 
+		[ { op = "close"; result = NextState "end" } ] 
+	};
+];;
+
+
+
+
+(* Mungo/examples/collection/CollectionProtocol.protocol *)
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "initialise"; result = NextState "Empty" } ] 
+	};
+	{ name = "Empty"; transitions = 
+		[ { op = "put"; result = NextState "NonEmpty" }; 
+			{ op = "close"; result = NextState "end" } ] 
+	};
+	{ name = "NonEmpty"; transitions = 
+		[ { op = "put"; result = NextState "NonEmpty" }; 
+			{ op = "get"; result = NextState "Unknown" } ] 
+	};
+	{ name = "Unknown"; transitions = 
+		[ { op = "put"; result = NextState "NonEmpty" }; 
+			{ op = "isEmpty"; result = Option [{ label = "TRUE"; state = NextState "Empty" }; { label = "FLASE"; state = NextState "NonEmpty" }] } ] 
+	};
+];;
+
+
+
+
+
+(* Mungo/examples/collection/StackUser.protocol *)
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "Stack produce(Stack, int)"; result = NextState "Consume" };
+			{ op = "Stack produce(Stack)"; result = NextState "Consume" };
+			{ op = "void close()"; result = NextState "end" } ]
+	};
+	{ name = "Consume"; transitions = 
+		[ { op = "Stack produce(Stack, int)"; result = NextState "Consume" };
+			{ op = "Stack produce(Stack)"; result = NextState "Consume" };
+			{ op = "Stack consume(Stack)"; result = NextState "Init" } ]
+	};
+];;
+
+
+
+
+
+(* Mungo/examples/TwoParties *)
+(* AliceProtocol *)
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "void connect()"; result = InnerState [{ op = "String recvStringFromBob()"; result = NextState "ReceiveChoice" }] } ] 
+	};
+	{ name = "ReceiveChoice"; transitions = 
+		[ { op = "BobChoice choiceFromBob()"; result = Option [{ label = "TIME"; state = NextState "SendTime"}; { label = "GREET"; state = NextState "HowAreYou" }] } ] 
+	};
+	{ name = "SendTime"; transitions = 
+		[ { op = "void sendTimeToBob(int)"; result = NextState "EndProtocol" } ] 
+	};
+	{ name = "HowAreYou"; transitions = 
+		[ { op = "void sendGreetToBob(String)"; result = NextState "EndProtocol" } ] 
+	};
+	{ name = "EndProtocol"; transitions = 
+		[ { op = "void endCommunication()"; result = NextState "end" } ] 
+	};
+];;
+
+(* BobProtocol *)
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "void connect()"; result = InnerState [{ op = "void sendStringToAlice(String)"; result = NextState "SendChoice" }] } ] 
+	};
+	{ name = "SendChoice"; transitions = 
+		[ { op = "void sendTimeChoiceToAlice()"; result = InnerState [{ op = "int recvTimeFromAlice()"; result = NextState "EndProtocol" }] };
+		  { op = "void sendGreetingChoiceToAlice()"; result = InnerState [{ op = "String recvGreetingFromAlice()"; result = NextState "EndProtocol" }] } ] 
+	};
+	{ name = "EndProtocol"; transitions = 
+		[ { op = "void endCommunication()"; result = NextState "end" } ] 
+	};
+];;
+
+
+
+
+
+(* Mungo/examples/ThreeParties *)
+(* BobProtocol *)
+let ts = [
+	{ name = "Connect"; transitions = 
+		[ { op = "void connect()"; result = NextState "GreetAlice" } ] 
+	};
+	{ name = "GreetAlice"; transitions = 
+		[ { op = "void sendHelloToAlice(String)"; result = NextState "GreetCarol" } ] 
+	};
+	{ name = "GreetCarol"; transitions = 
+		[ { op = "void sendHelloToCarol(String)"; result = NextState "MakeChoice" } ] 
+	};
+	{ name = "MakeChoice"; transitions = 
+		[ { op = "void sendTimeChoiceToAlice()"; result = InnerState [{ op = "int recvTimeFromAlice()"; result = NextState "EndProtocol" }] };
+		  { op = "void sendTimeChoiceToCarol()"; result = InnerState [{ op = "int recvTimeFromCarol()"; result = NextState "EndProtocol" }] } ] 
+	};
+	{ name = "EndProtocol"; transitions = 
+		[ { op = "void endCommunication()"; result = NextState "{}" } ] 
+	};
+];;
+
+(* FriendProtocol *)
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "void connect()"; result = InnerState [{ op = "String recvHelloFromBob()"; result = NextState "ReceiveChoice" }] } ] 
+	};
+	{ name = "ReceiveChoice"; transitions = 
+		[ { op = "BobChoice recvChoiceFromBob()"; result = Option [{ label = "TIME"; state = NextState "SendTime"}; { label = "END"; state = NextState "EndProtocol" }] } ] 
+	};
+	{ name = "SendTime"; transitions = 
+		[ { op = "void sendTimeToBob(int)"; result = NextState "EndProtocol" } ] 
+	};
+	{ name = "EndProtocol"; transitions = 
+		[ { op = "void endCommunication()"; result = NextState "end" } ] 
+	};
+];;
+
+
+
+
+
+
+(* Dimitris-Kouzapas/javatypestate/blob/master/demos/redis/channeloriented/RedisClientProtocol.protocol *)
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "void sendWATCH(String[])"; result = NextState "Watching" } ] 
+	};
+	{ name = "Watching"; transitions = 
+		[ { op = "WatchingLabel sendLabel_Watching(int)"; result = 
+				Option [{ label = "GET"; state = InnerState [{ op = "void sendArg_GET(String)"; result =
+									InnerState [{ op = "String receiveGET_response()"; result = NextState "Watching" }] }] }; 
+								{ label = "WATCH"; state = InnerState [{ op = "void sendArg_WATCH(String[])"; result = NextState "Watching" }] };
+								{ label = "MULTI"; state = NextState "Queued" }] 
+			} ] 
+	};
+	{ name = "Queued"; transitions = 
+		[ { op = "QueuedLabel sendLabel_Queued(int)"; result = 
+				Option [{ label = "SET"; state = InnerState [{ op = "void sendArg0_SET(String)"; result = 
+						InnerState [{ op = "void sendArg1_SET(String)"; result = NextState "Queued" }] }] }; 
+								{ label = "DISCARD"; state = NextState "end" };
+								{ label = "EXEC"; state = InnerState [{ op = "ResultLabel receiveEXEC_response()"; result = 
+									Option [{ label = "OK"; state = NextState"end"}; { label = "FAIL"; state = NextState "Queued" }] }] }] 
+			} ]
+	}
+];;
+
+
+
+
+
+
+
+(* ours *)
+
+(* jdmota/behaviour-types-research/blob/infinite/src/BankProtocol.protocol *)
+let ts = [
+	{ name = "State0"; transitions = 
+		[ { op = "void receivePayment()"; result = NextState "State0" } ] 
+	};
+];;
+
+(* jdmota/behaviour-types-research/blob/working-infinite/src/BankProtocol.protocol *)
+let ts = [
+	{ name = "State0"; transitions = 
+		[ { op = "void receivePayment()"; result = NextState "State0" };
+			{ op = "void exit()"; result = NextState "end" } ] 
+	};
+];;
+
+(* created for testing *)
+let ts = [
+	{ name = "State0"; transitions = [] }
+];;
+
+let ts = [
+	{ name = "State0"; transitions = [] };
+	{ name = "State1"; transitions = [] }
+];;
+
+
+
+
+
+(* ERRORS *)
+
+(* Should return "State with name 'end' not allowed." *)
+let ts = [
+	{ name = "end"; transitions = [] };
+];;
+
+(* Should return "Found duplicate state" *)
+let ts = [
+	{ name = "State0"; transitions = [] };
+	{ name = "State0"; transitions = [] };
+];;
+
+(* Should return "Undifined state: State1" *)
+let ts = [
+	{ name = "State0"; transitions = 
+		[ { op = "void doSomething()"; result = NextState "State1" } ] 
+	};
+];;
+
+(* Should return "There must be at least an option" *)
+let ts = [
+	{ name = "State0"; transitions = 
+		[ { op = "void doSomething()"; result = Option [] } ] 
+	};
+];;
+
+(* Should return "Internal choice states must always transition to external choice states" *)
+let ts = [
+	{ name = "State0"; transitions = 
+		[ { op = "void doSomething()"; result = Option [{ label = "L"; state = Option [{ label = "T"; state = NextState "end" }] }] } ] 
+	};
+];;
+
+(* "Found duplicate method or label: void close()" *)
+let ts = [
+	{ name = "State0"; transitions = 
+		[ { op = "void open()"; result = NextState "end" };
+			{ op = "void open(A)"; result = NextState "end" };
+			{ op = "void open(A,B)"; result = NextState "end" } ] 
+	};
+	{ name = "State1"; transitions = 
+		[ { op = "void close()"; result = NextState "end" };
+			{ op = "void close()"; result = NextState "State0" } ] 
+	}
+];;
+
+(* "Found duplicate method or label: OK" *)
+let ts = [
+	{ name = "Init"; transitions = 
+		[ { op = "open"; result = Option [{ label = "OK"; state = NextState "end" }; { label = "OK"; state = NextState "end" }; { label = "ERROR"; state = NextState "end" }] } ] 
+	};
+];;
